5f2f1f7c9a211d2d2b9d9b3bfb4dce0e6992b8de
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-09-23 10:24:24 +1000

    Add random score generation to score status program

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,25 +3,29 @@ CP1404 - Practical 02
 Ari Ralph
 Program to determine score status
 """
+import random
 
 
 def main():
-    """Print result from an inputted score"""
-    score = float(input("Enter score: "))
-    result = get_result(score)
-    print(f"The result for a score of {score} is {result}")
+    """Print result from an inputted score and a random score"""
+    inputted_score = float(input("Enter score: "))
+    inputted_score_result = get_result(inputted_score)
+    print(f"The result for a score of {inputted_score} is {inputted_score_result}.")
+    random_score = random.uniform(0, 100)
+    random_result = get_result(random_score)
+    print(f"The result for a random score of {random_score:.2f} is {random_result}.")
 
 
 def get_result(score: float) -> str:
     """Determine the result from a given score"""
     if score < 0 or score > 100:
-        return "Invalid"
+        return "invalid"
     elif score >= 90:
-        return "Excellent"
+        return "excellent"
     elif score >= 50:
-        return "Passable"
+        return "passable"
     else:
-        return "Bad"
+        return "bad"
 
 
 main()
