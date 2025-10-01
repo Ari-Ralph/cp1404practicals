48260cd3c34b29b6e0d9d025a06b4d534408d86a
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-01 17:21:00 +1000

    Answer docstring questions in the exceptions demo program

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,8 +2,12 @@
 CP1404 - Practical 03
 Answer the following questions:
 1. When will a ValueError occur?
+A Value Error will occur when the input is not an integer.
 2. When will a ZeroDivisionError occur?
+A ZeroDivisionError will occur when the denominator input is 0.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+To avoid the possibility of a ZeroDivisionError, a while loop could be added to ensure the denominator is
+not equal to 0.
 """
 
 try:
