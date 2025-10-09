4036165d36a5c2ecad670bfcb7db5c184817b735
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-09 14:09:02 +1000

    Add subject reader code and text file in prac 04

---
+++ prac_04/subject_reader.py
@@ -0,0 +1,30 @@
+"""
+CP1404 - Practical 04
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    """Read subject data from file and print it."""
+    data = load_data(FILENAME)
+    print(data)
+
+
+def load_data(filename=FILENAME):
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    input_file = open(filename)
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        print("----------")
+    input_file.close()
+
+
+main()
