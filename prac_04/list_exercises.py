eee4ee2c9f3b1eebd8e6966f31387c30b97893f8
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-09 14:22:57 +1000

    Add for loop to list exercises in prac 04

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,10 @@
+"""
+CP1404 - Practical 04
+Program to analysis 5 numbers
+"""
+NUMBER_OF_INPUTS = 5
+numbers = []
+for i in range(NUMBER_OF_INPUTS):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(numbers)
