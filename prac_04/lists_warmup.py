7be1997ca4f8455326f5cd991826411be23dab2e
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-09 13:36:25 +1000

    Add comments for expression results and complete part 2 for prac_04

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,11 +1,27 @@
+"""
+CP1404 - Practical 04
+List Warmup Exercise
+"""
+
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0]
-numbers[-1]
-numbers[3]
-numbers[:-1]
-numbers[3:4]
-5 in numbers
-7 in numbers
-"3" in numbers
-numbers + [6, 5, 3]
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] = 3, 1, 4, 1, 5, 9
+# numbers[3:4] = 1
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False
+# numbers + [6, 5, 3] = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+# Change the first element of numbers to "ten"
+numbers[0] = "ten"
+print(numbers)  # Print to visualise change
+# Change the last element of numbers to 1
+numbers[-1] = 1
+print(numbers)  # Print to visualise change
+# Print all the elements from numbers except the first two (slice)
+print(numbers[2:])
+# Print whether 9 is an element of numbers
+print(9 in numbers)
