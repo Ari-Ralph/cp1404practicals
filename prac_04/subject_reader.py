228fdcc52ff48f198f1285571acff8efead96f30
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-09 14:17:48 +1000

    Change load data function to return data and print  formatted data in prac 04

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,23 +8,23 @@ FILENAME = "subject_data.txt"
 
 def main():
     """Read subject data from file and print it."""
-    data = load_data(FILENAME)
-    print(data)
+    subject_data = load_data(FILENAME)
+
+    for subject in subject_data:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject_data = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_data.append(parts)
     input_file.close()
+    return subject_data
 
 
 main()
