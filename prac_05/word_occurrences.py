8545b27582ea297e008a8ee0bd2bef15bb582d7d
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-16 14:06:00 +1000

    Complete word occurrences program with actual time

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,5 +2,14 @@
 CP1404 - Practical 05
 Word Occurrences
 Estimate: 40 minutes
-Actual:
+Actual: 9 minutes 26 seconds
 """
+
+text_words = sorted(input("Text: ").lower().split())
+word_to_count = {}
+for word in text_words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+word_spacing = max(len(word) for word in text_words)
+for word in word_to_count:
+    print(f"{word:{word_spacing}} : {word_to_count[word]}")
