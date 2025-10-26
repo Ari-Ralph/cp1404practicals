f2ad87fa82e2ad936479261af1e08a67491638df
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-26 20:24:55 +1000

    Add programming languages files with time estimates

---
+++ prac_06/languages.py
@@ -0,0 +1,6 @@
+"""
+CP1404 - Practical 06
+Programming languages client file.
+Estimate: 80 minutes
+Actual
+"""
---
+++ prac_06/programming_language.py
@@ -0,0 +1,6 @@
+"""
+CP1404 - Practical 06
+Programming languages class file.
+Estimate: 80 minutes
+Actual:
+"""
