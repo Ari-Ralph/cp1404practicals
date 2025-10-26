f6015109085b6b2db8d704aca01dbf93fff2e32e
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-26 20:36:40 +1000

    Add init and is_dynamic methods to programming languages

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,3 +4,18 @@ Programming languages class file.
 Estimate: 80 minutes
 Actual:
 """
+
+
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection=False, year=""):
+        """Initialise a programming language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Is the language's typing dynamic."""
+        if self.typing == "Dynamic":
+            return True
+        return False
