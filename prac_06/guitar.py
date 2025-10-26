49718f2ea5b2ba4928b21a27ddf15182e7d212df
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-26 20:55:04 +1000

    Add guitar class files with estimates

---
+++ prac_06/guitar.py
@@ -0,0 +1,6 @@
+"""
+CP1404 - Practical 06
+Guitar class file
+Estimate: 40 minutes
+Actual:
+"""
---
+++ prac_06/guitar_test.py
@@ -0,0 +1,6 @@
+"""
+CP1404 - Practical 06
+Guitar class testing file
+Estimate: 40 minutes
+Actual:
+"""
