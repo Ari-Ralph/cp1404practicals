3d24325de452533ee3a30261c1189a6b11a3798c
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-10-26 19:53:06 +1000

    Complete parts 1-3 of cars modification exercise in prac 06

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,7 +1,5 @@
 """
 CP1404 Practical 06 - Client code to use the Car class.
-Note that the import has a folder (module) in it.
-This is why we name our folders with no spaces or capitals, as valid module names.
 """
 
 from prac_06.car import Car
@@ -13,6 +11,10 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+
+    limo = Car(100)
+    limo.add_fuel(20)
+    print(f"The limo has {limo.fuel}L of fuel.")
 
 
 main()
