831a30281bf41a547e33bfb9393d652884e6acba
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-11-02 19:51:43 +1000

    Add Project class with init method

--- prac_07/project.py
+++ prac_07/project.py
@@ -4,3 +4,13 @@ Project class file.
 Estimate: 2 hours
 Actual:
 """
+
+class Project:
+    """Project class to store information about a project"""
+    def __init__(self,name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise a project object"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
