af11b89bccc0537cce839a470f468ab516a4ccbd
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-11-02 20:17:56 +1000

    Add display projects function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -8,16 +8,15 @@ Actual:
 from prac_07.project import Project
 
 FILENAME = "projects.txt"
-MENU = "\n- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by da\n- (A)dd new project\n- (U)pdate proje\n- (Q)uit"
+MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by da\n- (A)dd new project\n- (U)pdate proje\n- (Q)uit"
 INDEX_COST_ESTIMATE = 3
 INDEX_COMPLETION_PERCENTAGE = 4
 
 def main():
     """Menu-based project management program."""
     projects = load_projects()
-    for project in projects:
-        print(project)
     print("Welcome to Pythonic Project Management")
+    print(f"Loaded {len(projects)} projects from {FILENAME}")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
@@ -26,13 +25,13 @@ def main():
         elif choice == "S":
             pass
         elif choice == "D":
-            pass
+            display_projects(projects)
         elif choice == "F":
             pass
         elif choice == "A":
             pass
         elif choice == "U":
-            pass
+            update_project(projects)
         else:
             print()
         print(MENU)
@@ -48,5 +47,21 @@ def load_projects():
             project_data = line.strip().split("\t")
             projects.append(Project(*project_data))
     return projects
+
+def display_projects(projects):
+    """Display projects organised into completion status."""
+    completed_projects = []
+    incomplete_projects = []
+    for project in projects:
+        if project.is_complete():
+            completed_projects.append(project)
+        else:
+            incomplete_projects.append(project)
+    print("Incomplete projects: ")
+    for incomplete_project in incomplete_projects:
+        print(f"  {incomplete_project}")
+    print("Completed projects: ")
+    for completed_project in completed_projects:
+        print(f"  {completed_project}")
 
 main()
