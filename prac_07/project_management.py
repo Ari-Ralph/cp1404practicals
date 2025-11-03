5ca969afbab29a37d6a2940882b0375aebfcf112
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-11-03 21:45:16 +1000

    Add load projects from inputted file menu option

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,49 +4,56 @@ Project client file.
 Estimate: 2 hours
 Actual:
 """
-
+# Stopped at 8:43pm
 from prac_07.project import Project
 
-FILENAME = "projects.txt"
+DEFAULT_FILENAME = "projects.txt"
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by da\n- (A)dd new project\n- (U)pdate proje\n- (Q)uit"
 INDEX_COST_ESTIMATE = 3
 INDEX_COMPLETION_PERCENTAGE = 4
 
+
 def main():
     """Menu-based project management program."""
-    projects = load_projects()
+    projects = load_projects(DEFAULT_FILENAME)
     print("Welcome to Pythonic Project Management")
-    print(f"Loaded {len(projects)} projects from {FILENAME}")
+    print(f"Loaded {len(projects)} projects from {DEFAULT_FILENAME}")
     print(MENU)
-    choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "L":
+    menu_choice = input(">>> ").upper()
+    while menu_choice != "Q":
+        if menu_choice == "L":
+            in_filename = input("Input Filename: ")
+            try:
+                load_projects(in_filename)
+                print(f"Loaded {len(projects)} projects from {in_filename}")
+            except FileNotFoundError:
+                print("Invalid filename - Please check your spelling")
+        elif menu_choice == "S":
             pass
-        elif choice == "S":
-            pass
-        elif choice == "D":
+        elif menu_choice == "D":
             display_projects(projects)
-        elif choice == "F":
+        elif menu_choice == "F":
             pass
-        elif choice == "A":
+        elif menu_choice == "A":
             pass
-        elif choice == "U":
+        elif menu_choice == "U":
             update_project(projects)
         else:
             print()
         print(MENU)
-        choice = input(">>> ").upper()
+        menu_choice = input(">>> ").upper()
 
 
-def load_projects():
+def load_projects(filename):
     """Read a txt file to create a list of Project objects."""
     projects = []
-    with open(FILENAME, 'r') as in_file:
-        in_file.readline() # Skip headers
+    with open(filename, 'r') as in_file:
+        in_file.readline()  # Skip headers
         for line in in_file:
             project_data = line.strip().split("\t")
             projects.append(Project(*project_data))
     return projects
+
 
 def display_projects(projects):
     """Display projects organised into completion status."""
@@ -63,5 +70,34 @@ def display_projects(projects):
     print("Completed projects: ")
     for completed_project in completed_projects:
         print(f"  {completed_project}")
+
+
+def update_project(projects):
+    for i, project in enumerate(projects):
+        print(i, project)
+    project_choice = get_valid_number("Project choice:", 0, len(projects) - 1)
+    selected_project = projects[project_choice]
+    print(selected_project)
+    selected_project.completion_percentage = get_valid_number("New Percentage: ", 1, 100)
+    selected_project.priority = get_valid_number("New priority: ", 1, 100)
+
+
+def get_valid_number(prompt: str, minimum: int, maximum: int) -> int:
+    """Get a valid number."""
+    is_number_valid = False
+    while not is_number_valid:
+        try:
+            number = int(input(prompt))
+            if number < minimum:
+                print(f"Number must be >= {minimum}")
+            elif maximum is not None and number > maximum:
+                print(f"Number must be less than {maximum}")
+
+            else:
+                is_number_valid = True
+        except ValueError:
+            print("Invalid input - please enter valid number")
+    return number  # Ignore warning
+
 
 main()
