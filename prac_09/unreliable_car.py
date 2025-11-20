d3003cd0b0cc63c54167e2efcb910fea6fd528a5
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-11-20 14:25:31 +1000

    Add unreliable car class with testing

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,28 @@
+"""
+CP1404 - Practical 09
+Unreliable Car class.
+"""
+
+from car import Car
+from random import uniform
+
+class UnreliableCar(Car):
+    """Specialised version of a Car, that introduces reliability."""
+    def __init__(self, name, fuel, reliability):
+        """Initialise a Unreliable Car instance, based on the parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+
+    def drive(self, distance):
+        """Return distance driven, but determines if the car drive with reliability attribute."""
+        if self.reliability <= uniform(0,100):
+            distance = 0
+        else:
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self.odometer += distance
+        return distance
