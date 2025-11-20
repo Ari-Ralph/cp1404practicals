0808f07cdf9d9770fa1143040c71b1f1eaa9f7ac
Ari_Ralph <ari.ralph@my.jcu.edu.au>
2025-11-20 14:54:04 +1000

    Add silver service taxi with testing

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,23 @@
+"""
+CP1404 - Practical 09
+Silver Service Car class.
+"""
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi with fanciness pricing and additional fees."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a Silver Service Taxi, with parent class Taxi."""
+        super().__init__(name, fuel)
+        self.price_per_km = self.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string like a Taxi but with flagfall."""
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return price of fare with flagfall."""
+        return self.flagfall + self.price_per_km * self.current_fare_distance
